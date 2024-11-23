7c777578d9bf22b5b210849bac42b9e18046ef4d
Code-With-Aashish <tanujgaming87@gmail.com>
2024-11-23 18:47:35 +0530

    fix the cors packege to take the request from anyware

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// Load environment variables if not in production
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
 // Using Middlewares
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" })); 
+app.use(express.urlencoded({ limit: "50mb", extended: true })); 
 app.use(cookieParser());
+app.use(cors());
 
-// Importing Routes
+
 const post = require("./routes/post");
 const user = require("./routes/user");
 
-// Using Routes
+
 app.use("/api/v1", post);
 app.use("/api/v1", user);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
+
 module.exports = app;
